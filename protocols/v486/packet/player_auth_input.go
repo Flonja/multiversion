43ef6523bddebc642ad1e7e6f1707cf569d1dc6a
Flonja <20887403+Flonja@users.noreply.github.com>
2023-04-26 22:07:49 +0200

    Add packets

---
+++ protocols/v486/packet/player_auth_input.go
@@ -0,0 +1,80 @@
+package packet
+
+import (
+	"github.com/go-gl/mathgl/mgl32"
+	"github.com/sandertv/gophertunnel/minecraft/protocol"
+	"github.com/sandertv/gophertunnel/minecraft/protocol/packet"
+)
+
+// PlayerAuthInput is sent by the client to allow for server authoritative movement. It is used to synchronise
+// the player input with the position server-side.
+// The client sends this packet when the ServerAuthoritativeMovementMode field in the StartGame packet is set
+// to true, instead of the MovePlayer packet. The client will send this packet once every tick.
+type PlayerAuthInput struct {
+	// Pitch and Yaw hold the rotation that the player reports it has.
+	Pitch, Yaw float32
+	// Position holds the position that the player reports it has.
+	Position mgl32.Vec3
+	// MoveVector is a Vec2 that specifies the direction in which the player moved, as a combination of X/Z
+	// values which are created using the WASD/controller stick state.
+	MoveVector mgl32.Vec2
+	// HeadYaw is the horizontal rotation of the head that the player reports it has.
+	HeadYaw float32
+	// InputData is a combination of bit flags that together specify the way the player moved last tick. It
+	// is a combination of the flags above.
+	InputData uint64
+	// InputMode specifies the way that the client inputs data to the screen. It is one of the constants that
+	// may be found above.
+	InputMode uint32
+	// PlayMode specifies the way that the player is playing. The values it holds, which are rather random,
+	// may be found above.
+	PlayMode uint32
+	// GazeDirection is the direction in which the player is gazing, when the PlayMode is PlayModeReality: In
+	// other words, when the player is playing in virtual reality.
+	GazeDirection mgl32.Vec3
+	// Tick is the server tick at which the packet was sent. It is used in relation to
+	// CorrectPlayerMovePrediction.
+	Tick uint64
+	// Delta was the delta between the old and the new position. There isn't any practical use for this field
+	// as it can be calculated by the server itself.
+	Delta mgl32.Vec3
+	// ItemInteractionData is the transaction data if the InputData includes an item interaction.
+	ItemInteractionData protocol.UseItemTransactionData
+	// ItemStackRequest is sent by the client to change an item in their inventory.
+	ItemStackRequest protocol.ItemStackRequest
+	// BlockActions is a slice of block actions that the client has interacted with.
+	BlockActions []protocol.PlayerBlockAction
+}
+
+// ID ...
+func (pk *PlayerAuthInput) ID() uint32 {
+	return packet.IDPlayerAuthInput
+}
+
+func (pk *PlayerAuthInput) Marshal(io protocol.IO) {
+	io.Float32(&pk.Pitch)
+	io.Float32(&pk.Yaw)
+	io.Vec3(&pk.Position)
+	io.Vec2(&pk.MoveVector)
+	io.Float32(&pk.HeadYaw)
+	io.Varuint64(&pk.InputData)
+	io.Varuint32(&pk.InputMode)
+	io.Varuint32(&pk.PlayMode)
+	if pk.PlayMode == packet.PlayModeReality {
+		io.Vec3(&pk.GazeDirection)
+	}
+	io.Varuint64(&pk.Tick)
+	io.Vec3(&pk.Delta)
+
+	if pk.InputData&packet.InputFlagPerformItemInteraction != 0 {
+		io.PlayerInventoryAction(&pk.ItemInteractionData)
+	}
+
+	if pk.InputData&packet.InputFlagPerformItemStackRequest != 0 {
+		protocol.Single(io, &pk.ItemStackRequest)
+	}
+
+	if pk.InputData&packet.InputFlagPerformBlockActions != 0 {
+		protocol.SliceVarint32Length(io, &pk.BlockActions)
+	}
+}
---
+++ protocols/v486/packet/update_attributes.go
@@ -0,0 +1,32 @@
+package packet
+
+import (
+	"github.com/flonja/multiversion/protocols/v486/types"
+	"github.com/sandertv/gophertunnel/minecraft/protocol"
+	"github.com/sandertv/gophertunnel/minecraft/protocol/packet"
+)
+
+// UpdateAttributes is sent by the server to update an amount of attributes of any entity in the world. These
+// attributes include ones such as the health or the movement speed of the entity.
+type UpdateAttributes struct {
+	// EntityRuntimeID is the runtime ID of the entity. The runtime ID is unique for each world session, and
+	// entities are generally identified in packets using this runtime ID.
+	EntityRuntimeID uint64
+	// Attributes is a slice of new attributes that the entity gets. It includes attributes such as its
+	// health, movement speed, etc. Note that only changed attributes have to be sent in this packet. It is
+	// not required to send attributes that did not have their values changed.
+	Attributes []types.Attribute
+	// Tick is the server tick at which the packet was sent. It is used in relation to CorrectPlayerMovePrediction.
+	Tick uint64
+}
+
+// ID ...
+func (*UpdateAttributes) ID() uint32 {
+	return packet.IDUpdateAttributes
+}
+
+func (pk *UpdateAttributes) Marshal(io protocol.IO) {
+	io.Varuint64(&pk.EntityRuntimeID)
+	protocol.Slice(io, &pk.Attributes)
+	io.Varuint64(&pk.Tick)
+}
