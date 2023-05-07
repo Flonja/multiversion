9d6543dac99deb39a15647e9a33c4278dafdbecd
Flonja <20887403+Flonja@users.noreply.github.com>
2023-05-07 14:20:08 +0200

    Some protocol changes

--- protocols/v486/downgrader.go
+++ protocols/v486/downgrader.go
@@ -6,6 +6,7 @@ import (
 	"github.com/flonja/multiversion/protocols/latest"
 	"github.com/flonja/multiversion/protocols/v486/mappings"
 	"github.com/sandertv/gophertunnel/minecraft/protocol"
+	"math"
 )
 
 const legacyItemVersion = 71
@@ -94,3 +95,71 @@ func downgradeChunk(c *chunk.Chunk, oldFormat bool) {
 		}
 	}
 }
+
+// downgradeEntityMetadata downgrades entity metadata from latest version to legacy version.
+func downgradeEntityMetadata(data map[uint32]any) map[uint32]any {
+	data = downgradeKey(data)
+
+	var flag1, flag2 int64
+	if v, ok := data[protocol.EntityDataKeyFlags]; ok {
+		flag1 = v.(int64)
+	}
+	if v, ok := data[protocol.EntityDataKeyFlagsTwo]; ok {
+		flag2 = v.(int64)
+	}
+	if flag1 == 0 && flag2 == 0 {
+		return data
+	}
+
+	newFlag1 := flag1 & ^(^0 << (protocol.EntityDataFlagDash - 1))
+	lastHalf := flag1 & (^0 << protocol.EntityDataFlagDash)
+	lastHalf >>= 1
+	lastHalf &= math.MaxInt64
+
+	newFlag1 |= lastHalf
+
+	if flag2 != 0 {
+		newFlag1 ^= (flag2 & 1) << 63
+		flag2 >>= 1
+		flag2 &= math.MaxInt64
+
+		data[protocol.EntityDataKeyFlagsTwo] = flag2
+	}
+
+	data[protocol.EntityDataKeyFlags] = newFlag1
+	return data
+}
+
+// downgradeKey downgrades the latest key of an entity metadata map to the legacy key.
+func downgradeKey(data map[uint32]any) map[uint32]any {
+	newData := make(map[uint32]any)
+	for key, value := range data {
+		switch key {
+		case protocol.EntityDataKeyBaseRuntimeID:
+			key = 120
+		case protocol.EntityDataKeyFreezingEffectStrength:
+			key = 121
+		case protocol.EntityDataKeyBuoyancyData:
+			key = 122
+		case protocol.EntityDataKeyGoatHornCount:
+			key = 123
+		case protocol.EntityDataKeyMovementSoundDistanceOffset:
+			key = 125
+		case protocol.EntityDataKeyHeartbeatIntervalTicks:
+			key = 126
+		case protocol.EntityDataKeyHeartbeatSoundEvent:
+			key = 127
+		case protocol.EntityDataKeyPlayerLastDeathPosition:
+			key = 128
+		case protocol.EntityDataKeyPlayerLastDeathDimension:
+			key = 129
+		case protocol.EntityDataKeyPlayerHasDied:
+			key = 130
+		}
+		newData[key] = value
+	}
+	return newData
+}
+
+// TODO: add downgrade entity flags
+// TODO: add downgrade command params
--- protocols/v486/upgrader.go
+++ protocols/v486/upgrader.go
@@ -64,3 +64,64 @@ func upgradeItemInstance(input protocol.ItemInstance) protocol.ItemInstance {
 		Stack:          upgradeItem(input.Stack),
 	}
 }
+
+// TODO: add upgradeChunk
+
+// upgradeEntityMetadata upgrades entity metadata from legacy version to latest version.
+func upgradeEntityMetadata(data map[uint32]any) map[uint32]any {
+	data = upgradeKey(data)
+
+	var flag1, flag2 int64
+	if v, ok := data[protocol.EntityDataKeyFlags]; ok {
+		flag1 = v.(int64)
+	}
+	if v, ok := data[protocol.EntityDataKeyFlagsTwo]; ok {
+		flag2 = v.(int64)
+	}
+
+	flag2 <<= 1
+	flag2 |= (flag1 >> 63) & 1
+
+	newFlag1 := flag1 & ^(^0 << (protocol.EntityDataFlagDash - 1))
+	lastHalf := flag1 & (^0<<protocol.EntityDataFlagDash - 1)
+	lastHalf <<= 1
+	newFlag1 |= lastHalf
+
+	data[protocol.EntityDataKeyFlagsTwo] = flag2
+	data[protocol.EntityDataKeyFlags] = newFlag1
+	return data
+}
+
+// upgradeKey upgrades the legacy key of an entity metadata map to the latest key.
+func upgradeKey(data map[uint32]any) map[uint32]any {
+	newData := make(map[uint32]any)
+	for key, value := range data {
+		switch key {
+		case 120:
+			key = protocol.EntityDataKeyBaseRuntimeID
+		case 121:
+			key = protocol.EntityDataKeyFreezingEffectStrength
+		case 122:
+			key = protocol.EntityDataKeyBuoyancyData
+		case 123:
+			key = protocol.EntityDataKeyGoatHornCount
+		case 125:
+			key = protocol.EntityDataKeyMovementSoundDistanceOffset
+		case 126:
+			key = protocol.EntityDataKeyHeartbeatIntervalTicks
+		case 127:
+			key = protocol.EntityDataKeyHeartbeatSoundEvent
+		case 128:
+			key = protocol.EntityDataKeyPlayerLastDeathPosition
+		case 129:
+			key = protocol.EntityDataKeyPlayerLastDeathDimension
+		case 130:
+			key = protocol.EntityDataKeyPlayerHasDied
+		}
+		newData[key] = value
+	}
+	return newData
+}
+
+// TODO: add upgrade entity flags
+// TODO: add upgrade command params
