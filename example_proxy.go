5287fe93ce7e1987e95d1bad3c0cc2fe253f1e8f
Flonja <20887403+Flonja@users.noreply.github.com>
2023-07-02 18:09:19 +0200

    Have two examples/use cases

---
+++ example_proxy.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"errors"
+	_ "github.com/flonja/multiversion/protocols" // VERY IMPORTANT
+	v582 "github.com/flonja/multiversion/protocols/v582"
+	"github.com/sandertv/gophertunnel/minecraft"
+	"sync"
+)
+
+// The following program implements a proxy that forwards players from one local address to a remote address.
+func runProxy(config config) {
+	p, err := minecraft.NewForeignStatusProvider(config.Connection.RemoteAddress)
+	if err != nil {
+		panic(err)
+	}
+	listener, err := minecraft.ListenConfig{
+		StatusProvider:    p,
+		AcceptedProtocols: []minecraft.Protocol{v582.New()},
+	}.Listen("raknet", config.Connection.LocalAddress)
+	if err != nil {
+		panic(err)
+	}
+	defer listener.Close()
+	for {
+		c, err := listener.Accept()
+		if err != nil {
+			panic(err)
+		}
+		go handleConn(c.(*minecraft.Conn), listener, config)
+	}
+}
+
+// handleConn handles a new incoming minecraft.Conn from the minecraft.Listener passed.
+func handleConn(conn *minecraft.Conn, listener *minecraft.Listener, config config) {
+	serverConn, err := minecraft.Dialer{
+		KeepXBLIdentityData: true,
+		IdentityData:        conn.IdentityData(),
+		ClientData:          conn.ClientData(),
+	}.Dial("raknet", config.Connection.RemoteAddress)
+	if err != nil {
+		panic(err)
+	}
+	var g sync.WaitGroup
+	g.Add(2)
+	go func() {
+		if err := conn.StartGame(serverConn.GameData()); err != nil {
+			panic(err)
+		}
+		g.Done()
+	}()
+	go func() {
+		if err := serverConn.DoSpawn(); err != nil {
+			panic(err)
+		}
+		g.Done()
+	}()
+	g.Wait()
+
+	go func() {
+		defer listener.Disconnect(conn, "connection lost")
+		defer serverConn.Close()
+		for {
+			pk, err := conn.ReadPacket()
+			if err != nil {
+				return
+			}
+			if err := serverConn.WritePacket(pk); err != nil {
+				if disconnect, ok := errors.Unwrap(err).(minecraft.DisconnectError); ok {
+					_ = listener.Disconnect(conn, disconnect.Error())
+				}
+				return
+			}
+		}
+	}()
+	go func() {
+		defer serverConn.Close()
+		defer listener.Disconnect(conn, "connection lost")
+		for {
+			pk, err := serverConn.ReadPacket()
+			if err != nil {
+				if disconnect, ok := errors.Unwrap(err).(minecraft.DisconnectError); ok {
+					_ = listener.Disconnect(conn, disconnect.Error())
+				}
+				return
+			}
+			if err := conn.WritePacket(pk); err != nil {
+				return
+			}
+		}
+	}()
+}
+
+type config struct {
+	Connection struct {
+		LocalAddress  string
+		RemoteAddress string
+	}
+}
