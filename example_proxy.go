dafa7a301e27ff2e99a2ccc889dea47641940074
Flonja <20887403+Flonja@users.noreply.github.com>
2023-07-23 20:20:57 +0200

    Add token saving to example

--- example_proxy.go
+++ example_proxy.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	_ "github.com/flonja/multiversion/protocols" // VERY IMPORTANT
 	v486 "github.com/flonja/multiversion/protocols/v486"
@@ -9,6 +10,7 @@ import (
 	"github.com/sandertv/gophertunnel/minecraft"
 	"github.com/sandertv/gophertunnel/minecraft/auth"
 	"golang.org/x/oauth2"
+	"os"
 	"sync"
 )
 
@@ -16,11 +18,7 @@ import (
 func runProxy(config config) {
 	var src oauth2.TokenSource
 	if config.AuthEnabled {
-		token, err := auth.RequestLiveToken()
-		if err != nil {
-			panic(err)
-		}
-		src = auth.RefreshTokenSource(token)
+		src = tokenSource()
 	}
 
 	p, err := minecraft.NewForeignStatusProvider(config.Connection.RemoteAddress)
@@ -112,4 +110,35 @@ type config struct {
 		RemoteAddress string
 	}
 	AuthEnabled bool
+}
+
+// tokenSource returns a token source for using with a gophertunnel client. It either reads it from the
+// token.tok file if cached or requests logging in with a device code.
+func tokenSource() oauth2.TokenSource {
+	check := func(err error) {
+		if err != nil {
+			panic(err)
+		}
+	}
+	token := new(oauth2.Token)
+	tokenData, err := os.ReadFile("token.tok")
+	if err == nil {
+		_ = json.Unmarshal(tokenData, token)
+	} else {
+		token, err = auth.RequestLiveToken()
+		check(err)
+	}
+	src := auth.RefreshTokenSource(token)
+	_, err = src.Token()
+	if err != nil {
+		// The cached refresh token expired and can no longer be used to obtain a new token. We require the
+		// user to log in again and use that token instead.
+		token, err = auth.RequestLiveToken()
+		check(err)
+		src = auth.RefreshTokenSource(token)
+	}
+	tok, _ := src.Token()
+	b, _ := json.Marshal(tok)
+	_ = os.WriteFile("token.tok", b, 0644)
+	return src
 }
